1d37e145722a15f0d3fcb9e5182ef63103824027
BlackChao-Script <83378274+BlackChao-Script@users.noreply.github.com>
2021-06-05 11:56:30 +0800

    master

---
+++ src/components/common/swiper/index.js
@@ -0,0 +1,6 @@
+import Swiper from './Swiper.vue'
+import SwiperItem from './Swiperltem.vue'
+
+export {
+  Swiper, SwiperItem
+}
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,28 +3,39 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item, index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 import navBar from "_c/common/navbar/navBar";
 import { getHomeMultidata } from "@/network/home";
+import { Swiper, SwiperItem } from "_c/common/swiper";
 
 export default {
   name: "home",
   data() {
     return {
-      result: null,
+      banners: [],
+      recommends: [],
     };
   },
   components: {
     navBar,
+    Swiper,
+    SwiperItem,
   },
   created() {
     //1.请求多个数据
     getHomeMultidata().then((res) => {
-      console.log(res);
-      this.result = res;
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
     });
   },
 };
