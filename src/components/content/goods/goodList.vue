9cc98330ca54a770b22d356c7630037222b65403
BlackChao-Script <83378274+BlackChao-Script@users.noreply.github.com>
2021-06-06 17:07:17 +0800

    home页面制作中

---
+++ src/components/content/goods/goodList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="goodslist">
+    <goods-list-item
+      v-for="(item, index) in goods"
+      :key="index"
+      :goods-item="item"
+    >
+    </goods-list-item>
+  </div>
+</template>
+
+<script>
+import goodsListItem from "./goodListItem";
+
+export default {
+  name: "goodsList",
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  components: {
+    goodsListItem,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.goodslist {
+  display: flex;
+  // 多余元素换行显示
+  flex-wrap: wrap;
+  // 等比分部空间
+  justify-content: space-evenly;
+}
+</style>
---
+++ src/components/content/goods/goodListItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="goodsItem">
+    <img :src="goodsItem.show.img" alt="" />
+    <div class="goods-info">
+      <p>{{ goodsItem.title }}</p>
+      <span class="price">{{ goodsItem.price }}</span>
+      <span class="iconfont icon-xiaoxingxing"></span>
+      <span class="collect">{{ goodsItem.cfav }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "goodListItem",
+  props: {
+    goodsItem: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.goodsItem {
+  width: 46%;
+}
+.goodsItem img {
+  width: 100%;
+  border-radius: 5px;
+}
+.goods-info {
+  font-size: 12px;
+  overflow: hidden;
+  text-align: center;
+}
+.goods-info p {
+  //多余文字不换行
+  white-space: nowrap;
+  // 配合下面属性用的
+  overflow: hidden;
+  // 隐藏文字多余文字用...代替
+  text-overflow: ellipsis;
+}
+.price{
+  color: var(--color-high-text);
+}
+.iconfont{
+ font-size: 14px;
+ margin: 0 2px;
+}
+</style>
